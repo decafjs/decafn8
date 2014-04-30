8ddd33daea1f23b9a561b595299ee5629078f6f9
Michael Schwartz <mykesx@gmail.com>
2014-04-30 06:17:26 -0700

    tweak

--- builtins/sync.js
+++ builtins/sync.js
@@ -7,7 +7,7 @@ function sync (func, obj) {
     var Lock = Java.type('java.util.concurrent.locks.ReentrantLock');
 
     if (arguments.length < 1 || arguments.length > 2 ) {
-        throw "sync(function [,object]) parameter count mismatch";
+        throw new Error("sync(function [,object]) parameter count mismatch");
     }
 
     var syncObject = (arguments.length == 2 ? obj : this);
@@ -20,7 +20,8 @@ function sync (func, obj) {
         syncObject._syncLock.lock();
         try {
             func.apply(null, arguments);
-        } finally {
+        }
+        finally {
             syncObject._syncLock.unlock();
         }
     };
